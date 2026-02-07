715b5c66631f85895615abb9f92910077896abd1
computronics-john <174473026+computronics-john@users.noreply.github.com>
2026-02-07 18:33:18 +0800

    Perform exercises about data types and conditionals

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(selectedString, numberOfIterations) {
+    let finalString = ""
+    let currentIteration = 0
+    while (currentIteration < numberOfIterations) {
+        finalString += selectedString
+        currentIteration++
+    }
+    if (numberOfIterations < 0) return "ERROR"
+    else return finalString
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(selectedString) {
+    let finalString = ""
+    let numberOfCharacters = selectedString.length
+    while (numberOfCharacters > 0) {
+        finalString += selectedString.charAt(numberOfCharacters - 1)
+        numberOfCharacters--    
+    }
 
+    return finalString
 };
 
 // Do not edit below this line
